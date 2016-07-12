eff6c66548109fc1e94e6e2c4024bad87c13fe1d
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-12 00:31:29 -0400

    Solve wildcard import in special.py.

--- qpecgen/special.py
+++ qpecgen/special.py
@@ -1,18 +1,25 @@
-from base import QpecgenProblem
-from helpers import *
+from .base import QpecgenProblem
+from . import helpers
 
 
 class QpecgenSpecialLCP(QpecgenProblem):
+
     def __init__(self, param, qpec_type=-1):
         super(QpecgenSpecialLCP, self).__init__(param, qpec_type=qpec_type)
         m = self.param['m']
         n = self.param['n']
         # type 800 is the 'Good LCP-MPEC' and type 900 is the 'Bad LCP-MPEC'
-        self.ygen = zeros(m)
-        self.a = zeros(self.param['l'])
-        self.N = conmat([-eye(n), zeros(m - n, n)], option='v')
-        self.M = eye(m)
-        self.q = zeros(m)
+        self.ygen = helpers.zeros(m)
+        self.a = helpers.zeros(self.param['l'])
+        self.N = helpers.conmat(
+            [-helpers.eye(n), helpers.zeros(m - n, n)], option='v')
+        self.M = helpers.eye(m)
+        self.q = helpers.zeros(m)
+
+        # define initial values.
+        self.c = None
+        self.d = None
+        self.xgen = None
 
     def return_problem(self):
         problem = {
@@ -24,28 +31,30 @@ class QpecgenSpecialLCP(QpecgenProblem):
             'N': self.N,
             'M': self.M,
             'q': self.q}
-        optsolxy = conmat([self.xgen, self.ygen])
+        optsolxy = helpers.conmat([self.xgen, self.ygen])
         info = {
             'xgen': self.xgen,
             'ygen': self.ygen,
             'optsol': optsolxy,
-            'optval': (0.5 * (optsolxy.T) * self.P * optsolxy + \
-                        conmat([self.c, self.d]).T * optsolxy)[0, 0]
+            'optval': (0.5 * (optsolxy.T) * self.P * optsolxy +
+                       helpers.conmat([self.c, self.d]).T * optsolxy)[0, 0]
         }
         return problem, info, self.param
 
 
 class Qpecgen800(QpecgenSpecialLCP):
+
     def __init__(self, param):
         super(Qpecgen800, self).__init__(param, qpec_type=800)
-        self.c = -2. * ones(self.n)
-        self.d = 4. * ones(self.m)
-        self.xgen = zeros(self.n)
+        self.c = -2. * helpers.ones(self.n)
+        self.d = 4. * helpers.ones(self.m)
+        self.xgen = helpers.zeros(self.n)
 
 
 class Qpecgen900(QpecgenSpecialLCP):
+
     def __init__(self, param):
         super(Qpecgen900, self).__init__(param, qpec_type=900)
-        self.c = 2. * ones(self.n)
-        self.d = -4. * ones(self.m)
-        self.xgen = -ones(self.n)
+        self.c = 2. * helpers.ones(self.n)
+        self.d = -4. * helpers.ones(self.m)
+        self.xgen = -helpers.ones(self.n)
