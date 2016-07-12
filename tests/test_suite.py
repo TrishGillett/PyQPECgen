4cf2f27f000e8cfeb19795c2af19973cf6b009a3
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-07-11 23:22:32 -0400

    Add a factory of the test suite.

---
+++ tests/test_suite.py
@@ -0,0 +1,37 @@
+""" Test suite.
+"""
+import sys
+import unittest
+
+from . import qpecgen_test
+
+
+def suite():
+    """ Returns a test suite.
+    """
+    loader = unittest.TestLoader()
+    res = unittest.TestSuite()
+
+    res.addTest(loader.loadTestsFromModule(qpecgen_test))
+    return res
+
+
+def main():
+    """ The main function.
+
+    Returns:
+      Status code.
+    """
+    try:
+        res = unittest.TextTestRunner(verbosity=2).run(suite())
+
+    except KeyboardInterrupt:
+        print("Test canceled.")
+        return -1
+
+    else:
+        return 0 if res.wasSuccessful() else 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
